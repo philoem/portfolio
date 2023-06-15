d433c0edc00ccc7d49130f705e39e78167764cef
Philoem <philoem24@gmail.com>
2023-06-15 16:24:07 +0200

    style: remove unused eslint+prettier+add rome

--- src/root.tsx
+++ src/root.tsx
@@ -5,24 +5,24 @@ import { RouterHead } from './components/router-head/router-head';
 import './global.css';
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Dont remove the `<head>` and `<body>` elements.
-   */
+	/**
+	 * The root of a QwikCity site always start with the <QwikCityProvider> component,
+	 * immediately followed by the document's <head> and <body>.
+	 *
+	 * Dont remove the `<head>` and `<body>` elements.
+	 */
 
-  return (
-    <QwikCityProvider>
-      <head>
-        <meta charSet="utf-8" />
-        <link rel="manifest" href="/manifest.json" />
-        <RouterHead />
-      </head>
-      <body lang="en">
-        <RouterOutlet />
-        <ServiceWorkerRegister />
-      </body>
-    </QwikCityProvider>
-  );
+	return (
+		<QwikCityProvider>
+			<head>
+				<meta charSet='utf-8' />
+				<link rel='manifest' href='/manifest.json' />
+				<RouterHead />
+			</head>
+			<body lang='en'>
+				<RouterOutlet />
+				<ServiceWorkerRegister />
+			</body>
+		</QwikCityProvider>
+	);
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,30 +2,30 @@ import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
-  );
+	return (
+		<>
+			<h1>Hi 👋</h1>
+			<p>
+				Can't wait to see what you build with qwik!
+				<br />
+				Happy coding.
+			</p>
+		</>
+	);
 });
 
 export const head: DocumentHead = {
-  title: "Philoem's portfolio",
-  meta: [
-    {
-      name: "It's my Qwik-designed portfolio",
-      content: 'Portfolio built with Qwik',
-    },
-  ],
-  links: [
-    {
-      rel: 'canonical',
-      href: 'https://github.com/philoem',
-    }
-  ]
+	title: "Philoem's portfolio",
+	meta: [
+		{
+			name: "It's my Qwik-designed portfolio",
+			content: 'Portfolio built with Qwik'
+		}
+	],
+	links: [
+		{
+			rel: 'canonical',
+			href: 'https://github.com/philoem'
+		}
+	]
 };
