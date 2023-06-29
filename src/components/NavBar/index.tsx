8c8c09a87fe6f6c8fcde40a0788cf378f47fcbfa
Philoem <philoem24@gmail.com>
2023-06-29 11:51:46 +0200

    feat: add new style to navbar+remove unused style to global

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,20 +1,29 @@
 import { component$ } from '@builder.io/qwik';
+import styles from './index.module.css';
 
 export default component$(() => {
 	return (
-		<nav>
-			<ul>
+		<nav class={styles.navContainer}>
+			<ul class={styles.ulList}>
 				<li>
-					<a href='/about'>About</a>
+					<a href='/about' class={styles.aLink}>
+						<p id={styles.about}>About</p>
+					</a>
 				</li>
 				<li>
-					<a href='/experiences'>Experiences</a>
+					<a href='/experiences' class={styles.aLink}>
+						<p id={styles.experiences}>Experiences</p>
+					</a>
 				</li>
 				<li>
-					<a href='/projects'>Projects</a>
+					<a href='/projects' class={styles.aLink}>
+						<p id={styles.projects}>Projects</p>
+					</a>
 				</li>
 				<li>
-					<a href='/contact'>Contact</a>
+					<a href='/contact' class={styles.aLink}>
+						<p id={styles.contact}>Contact</p>
+					</a>
 				</li>
 			</ul>
 		</nav>
