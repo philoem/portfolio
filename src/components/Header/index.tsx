a09da026044c454ab4894c1a8c6d19f3915dfb45
Philoem <philoem24@gmail.com>
2023-06-29 11:52:30 +0200

    feat: add new component header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+	return (
+		<div>
+			<h1>Philippe Chamard</h1>
+			<p>Web & mobile developer front-end</p>
+		</div>
+	);
+});
