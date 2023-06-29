3805bee9a3095f20a068e1afa74f6b6437f5a534
Philoem <philoem24@gmail.com>
2023-06-29 11:50:21 +0200

    feat: custom routes with new pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,11 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import NavBar from '~/components/NavBar';
+import Home from '~/pages/Home';
 
 export default component$(() => {
 	return (
 		<>
-			<NavBar />
-			<h1>Hi 👋</h1>
-			<p>
-				Can't wait to see what you build with qwik!
-				<br />
-				Happy coding.
-			</p>
+			<Home />
 		</>
 	);
 });
